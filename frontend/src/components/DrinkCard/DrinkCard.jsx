8262bdc9c11c690804ab6f82f3080fb25858304e
Duc Anh Dang <danganhbill0806@gmail.com>
2026-02-02 01:50:52 -0600

    fixed: styling for menu page

--- frontend/src/components/DrinkCard/DrinkCard.jsx
+++ frontend/src/components/DrinkCard/DrinkCard.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from "react";
 import testImg from "../../assets/drinkTestImg.png";
-import "./DrinkCard.css"
+import "./DrinkCard.css";
 
-export default function DrinkCard ({data}) {
-    return (
-       <div className='drink-card'>
-         <img className='drink-img' src={testImg}></img>
-         <h3 className='drink-name'>{data.name}</h3>
-         <p className='drink-price'>{data.price}</p>
-       </div>
-    )
+export default function DrinkCard({ data }) {
+  return (
+    <div class="drink-card">
+      <div class="drink-img-container">
+        <img src={data.images[0]} alt="Cappuccino" class="drink-img" />
+      </div>
+      <div class="drink-info">
+        <h3 class="drink-name">{data.name}</h3>
+        <p class="drink-price">{data.price}đ</p>
+      </div>
+    </div>
+  );
 }
