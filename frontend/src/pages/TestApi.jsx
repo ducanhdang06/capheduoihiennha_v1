41712ffb53e93afee39ac74de137663bf086e2a0
Duc Anh Dang <danganhbill0806@gmail.com>
2026-02-16 00:20:48 -0600

    fix

--- frontend/src/pages/TestApi.jsx
+++ frontend/src/pages/TestApi.jsx
@@ -1,10 +1,10 @@
 import { useEffect } from "react";
 import { getDrinks } from "../api/drink.api";
-import { getCategoryDrinksMenu } from "../api/drink.api";
+import { getAllDrinks } from "../api/drink.api";
 
 export default function TestApi() {
   useEffect(() => {
-    getCategoryDrinksMenu ()
+    getAllDrinks ()
       .then(res => console.log("BACKEND DATA:", res.data))
       .catch(err => console.error("API ERROR:", err));
   }, []);
